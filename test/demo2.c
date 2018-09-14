7f2c856e1cffa5bda22c33635ae52958773e7fe6
Adem Budak <adembudak@outlook.com>
2018-09-14 20:28:12 +0300

    Added: tests for demonstrate correctness of the implementations

--- test/demo2.c
+++
@@ -1,9 +0,0 @@
-
-#define TYPE int
-
-static int cmp(const void *a, const void *b){
-  const TYPE da = *((const TYPE *) a);
-  const TYPE db = *((const TYPE *) b);
-  
-  return (da < db) ? 1 : (da == db) ? 0 : -1;
-}
